4bb92c9624d7abb90ca6a22f4ef363e7ec064d9a
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-21 21:59:55 +0600

    database connection has been setup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -5,6 +7,11 @@ const cors = require('cors');
 
 // middleware
 app.use(cors());
+
+// database setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
     res.send("Assessment app server is running");

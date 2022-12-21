0f6e01fa619c65bd2753c9514604434a83c72fc9
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-21 22:04:22 +0600

    added dataCollection GET api

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const dataCollections = client.db('assessmentDB').collection('dataTable');
+
+        // dataCollections [GET method]
+        app.get('/data', async (req, res) => {
+            const query = {};
+            const allData = await dataCollections.find(query).toArray();
+            res.send(allData);
+        })
+    }
+    finally { }
+}
+run().catch(error => console.error(error));
+
 
 app.get('/', (req, res) => {
     res.send("Assessment app server is running");
